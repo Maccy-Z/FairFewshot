23ae8d5fb383a4349cbd76656d241f7bc705dc60
Kaasiak <kasia.kobalczyk@gmail.com>
2023-05-02 22:31:53 +0100

    Fix bash file

--- run_models.bash
+++ run_models.bash
@@ -1,17 +1,17 @@
 for i in {0..9}
 do
-    python Fewshot/config.py --ds-group $i --num_rows 5 --num_targets 5
+    python Fewshot/config.py --ds-group $i --num-rows 5 --num-targets 5
     python Fewshot/main.py
 done
 
 for i in {0..9}
 do
-    python Fewshot/config.py --ds-group $i --num_rows 10 --num_targets 10
+    python Fewshot/config.py --ds-group $i --num-rows 10 --num-targets 10
     python Fewshot/main.py
 done
 
 for i in {0..9}
 do
-    python Fewshot/config.py --ds-group $i --num_rows 5 --num_targets 5
+    python Fewshot/config.py --ds-group $i --num-rows 5 --num-targets 5
     python Fewshot/main.py
 done
