45a6978be09221639409503744a060d89f100ef6
Kaasiak <kasia.kobalczyk@gmail.com>
2023-05-03 13:48:43 +0100

    Prepare 5-5 model

--- run_models.bash
+++ run_models.bash
@@ -4,12 +4,12 @@
 #     python Fewshot/main.py
 # done
 
-for i in {0..9}
-do
-    python Fewshot/config.py --ds-group $i --num-rows 10 --num-targets 10
-    python Fewshot/main.py
-done
-python Fewshot/comparison2.py
+# for i in {0..9}
+# do
+#     python Fewshot/config.py --ds-group $i --num-rows 10 --num-targets 10
+#     python Fewshot/main.py
+# done
+# python Fewshot/comparison2.py
 
 for i in {0..9}
 do
