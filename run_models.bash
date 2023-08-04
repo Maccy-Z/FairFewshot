2ec326645e972e0bd44bc601872ab224795386af
Kaasiak <kasia.kobalczyk@gmail.com>
2023-08-04 22:58:52 +0100

    Update bash

--- run_models.bash
+++ run_models.bash
@@ -1,5 +1,17 @@
-for i in {0..9}
+# for i in {0..4}
+# do
+#     python Fewshot/config.py --ds-group $i
+#     python Fewshot/main.py
+# done
+
+for n_overlap in 12 10 8 4 6 2 0
 do
-    python Fewshot/config.py --ds-group $i
-    python Fewshot/main.py
+    for seed in {0..5}
+    do
+        i=$((n_overlap * 10 + seed))
+        python Fewshot/config.py --ds-group $i
+        python Fewshot/main.py
+    done
 done
+
+python Fewshot/comparison2.pyadd
