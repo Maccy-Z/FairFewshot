ef22ae366b4e104de9f65e6e504188654d13c349
Kaasiak <kasia.kobalczyk@gmail.com>
2023-08-19 21:44:47 +0100

    Add ds group;

--- run_models.bash
+++ run_models.bash
@@ -3,6 +3,6 @@
 #     python Fewshot/config.py --ds-group $i
 #     python Fewshot/main.py
 # done
-python Fewshot/config.py --ds-group 0git 
+python Fewshot/config.py --ds-group 0
 python Fewshot/main.py
 python Fewshot/comparison2.py
